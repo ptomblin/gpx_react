80c7f46a724bac1bc354526bbe79bf11f83ba3a5
Paul Tomblin <ptomblin@allhats2.xcski.com>
2018-10-16 20:14:13 -0400

    Update dependencies. Haven't actually verified the functionality.

--- webpack.config.js
+++ webpack.config.js
@@ -7,17 +7,6 @@ module.exports = {
   devtool: debug ? "inline-sourcemap" : null,
   entry: "./js/client.js",
   module: {
-    loaders: [
-      {
-        test: /\.jsx?$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['react', 'es2015', 'stage-0'],
-          plugins: ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy'],
-        }
-      }
-    ]
   },
   output: {
     path: __dirname + "/src/",
